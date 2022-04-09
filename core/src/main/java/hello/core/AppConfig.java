473fb121dc63287cf912434fa1a52c66041a9098
hyeonwook98 <hyeonwook98@naver.com>
2022-04-09 20:19:47 +0900

    chore: 싱글톤 컨테이너 학습

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -12,24 +12,29 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class AppConfig {
 
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
     @Bean
     //MemberService(인터페이스)에 대한 구현은 MemberServiceImpl를 쓸것이다.
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     //MemberRepository(인터페이스)에 대한 구현은 MemoryMemberRepository를 쓸 것이다.
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     //OrderService(인터페이스)에 대한 구현은 OrderServiceImpl를 쓸 것이다.
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 
