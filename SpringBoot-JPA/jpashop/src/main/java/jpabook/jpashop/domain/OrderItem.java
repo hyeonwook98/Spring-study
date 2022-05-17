d0ce46e1cba8245c3264286734cdf2b494c4c135
hyeonwook98 <hyeonwook98@naver.com>
2022-05-17 11:30:36 +0900

    chore: 주문, 주문상품 엔티티 개발

--- SpringBoot-JPA/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ SpringBoot-JPA/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -24,4 +24,26 @@ public class OrderItem {
 
     private int orderPrice; //주문 가격
     private int count; //주문 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    //재고수량을 복귀시키는 부분
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
