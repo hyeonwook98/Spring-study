fcc688672beb31a7b35a7d0c0867d86d404af0db
hyeonwook98 <hyeonwook98@naver.com>
2022-05-21 13:04:56 +0900

    chore: 주문 서비스 개발

--- SpringBoot-JPA/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ SpringBoot-JPA/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,16 +1,21 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "order_item_id")
     private Long id;
 
@@ -24,6 +29,7 @@ public class OrderItem {
 
     private int orderPrice; //주문 가격
     private int count; //주문 수량
+
 
     //==생성 메서드==//
     public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
