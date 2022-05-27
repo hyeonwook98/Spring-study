cd3d06ae5f6457095c3b843d8519802e405343ac
hyeonwook98 <hyeonwook98@naver.com>
2022-05-27 09:22:17 +0900

    chore: 필드와 컬럼 매핑 학습

--- jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,12 +18,13 @@ public class JpaMain {
 
         try {
 
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAA");
+            Member member = new Member();
+            member.setId(2L);
+            member.setUsername("B");
+            member.setRoleType(RoleType.ADMIN);
 
-            em.detach(member);
+            em.persist(member);
 
-            System.out.println("============================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,26 +1,38 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 public class Member {
 
     @Id
     private Long id;
-    @Column(unique = true, length = 10)
-    private String name;
+
+    @Column(name = "name", nullable = false) //데이터베이스 컬럼명은 name이다.
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING) //enum타입을 쓰고싶을 때 사용
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP) //시간같은 경우 DB에서는 3가지로 구분하여사용한다. (날짜,시간,(날짜,시간))
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    @Lob // DB에 큰 데이터를 넣고싶을때 사용
+    private String description;
     public Member() {
 
     }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
 
     public Long getId() {
         return id;
@@ -30,11 +42,51 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
