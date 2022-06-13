77d27f3f77d580c3032391d86ac06e8d1683ea7c
hyeonwook98 <hyeonwook98@naver.com>
2022-06-13 19:50:09 +0900

    chore: 양방향 매핑시 주의점 학습

--- jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -24,18 +24,22 @@ public class JpaMain {
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeam(team);
             em.persist(member);
+
+            team.addMember(member);
 
             em.flush();
             em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());
-            List<Member> members = findMember.getTeam().getMembers();
+            Team findTeam = em.find(Team.class, team.getId()); //1차 캐시
+            List<Member> members = findTeam.getMembers();
 
+            System.out.println("===========");
             for (Member m : members) {
                 System.out.println("m = " + m.getUsername());
             }
+            System.out.println("===========");
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
