62efb9cb97e3d7bd85c336b695bc67b45791b1ae
hyeonwook98 <hyeonwook98@naver.com>
2022-06-13 18:57:59 +0900

    chore: 양방향 연관관계와 연관관계의 주인 학습

--- jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -27,11 +27,15 @@ public class JpaMain {
             member.setTeam(team);
             em.persist(member);
 
+            em.flush();
+            em.clear();
+
             Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
 
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam.getName() = " + findTeam.getName());
-
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
