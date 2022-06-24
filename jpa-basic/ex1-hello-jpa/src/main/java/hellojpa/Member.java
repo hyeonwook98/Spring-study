430e840dddcdfafdde219007a76ef36977372cc7
hyeonwook98 <hyeonwook98@naver.com>
2022-06-24 19:24:31 +0900

    chore: 다대다 연관관계 학습

--- jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,6 +1,8 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -19,6 +21,9 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
