d890078d3e64451d059e68ecbf47b4dd6103e8a3
hyeonwook98 <hyeonwook98@naver.com>
2022-06-27 09:34:35 +0900

    chore: 영속성 전이(CASCADE)와 고아 객체 학습

--- jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -20,30 +20,21 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
 
-            Team teamB = new Team();
-            team.setName("teamB");
-            em.persist(teamB);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team);
-            em.persist(member1);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setTeam(teamB);
-            em.persist(member2);
-
+            em.persist(parent);
+            
             em.flush();
             em.clear();
 
-            //Member m = em.find(Member.class, member1.getId());
-
-            List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+            Parent findParent = em.find(Parent.class, parent.getId());
+            em.remove(findParent);
 
             tx.commit();
         } catch (Exception e) {
