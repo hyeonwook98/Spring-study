1970f2569b51ec504da326f875ce4d0055900f8c
hyeonwook98 <hyeonwook98@naver.com>
2022-06-27 09:54:47 +0900

    chore: 연관관계 관리 실전 예제

--- jpa-basic/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ jpa-basic/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -4,6 +4,8 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 public class Category extends BaseEntitiy{
 
@@ -14,7 +16,7 @@ public class Category extends BaseEntitiy{
 
     private String name;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "PARENT_ID")
     private Category parent;
 
--- jpa-basic/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpa-basic/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -15,15 +15,15 @@ public class Order extends BaseEntitiy{
 
     //    @Column(name = "MEMBER_ID")
 //    private Long memberId;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "DELIVERY_ID")
     private Delivery delivery;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime orderDate;
