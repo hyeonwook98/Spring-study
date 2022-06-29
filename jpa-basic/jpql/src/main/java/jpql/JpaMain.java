1f5636e0e5a36584d290a07af34ddd6c22489cf7
hyeonwook98 <hyeonwook98@naver.com>
2022-06-29 16:20:40 +0900

    chore: JPQL - 기본 문법과 기능

---
+++ jpa-basic/jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,38 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            TypedQuery<Member> query = em.createQuery("select m from Member m where m.username = :username", Member.class);
+            query.setParameter("username", "member1");
+            Member singleResult = query.getSingleResult();
+            System.out.println("singleResult = " + singleResult.getUsername());
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close(); //실제 애플리케이션을 종료하고자 할때는 엔티티매니저팩토리를 종료해주어야한다.
+    }
+    
+}
