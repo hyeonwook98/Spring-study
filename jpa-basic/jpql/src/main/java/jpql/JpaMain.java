8414a056f3e5cdb5d3623b3e76e711a4b7422aba
hyeonwook98 <hyeonwook98@naver.com>
2022-07-02 16:23:31 +0900

    chore: 페이징 학습

--- jpa-basic/jpql/src/main/java/jpql/JpaMain.java
+++ jpa-basic/jpql/src/main/java/jpql/JpaMain.java
@@ -15,16 +15,25 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            for (int i =0; i<100;i++){
+                Member member = new Member();
+                member.setUsername("member1" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
 
-            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age)  from Member m", MemberDTO.class).getResultList();
+            em.flush();
+            em.clear();
 
-            MemberDTO memberDTO = resultList.get(0);
-            System.out.println("MemberDTO = " + memberDTO.getUsername());
-            System.out.println("MemberDTO = " + memberDTO.getAge());
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            System.out.println("result.size = " + result.size());
+            for (Member member1 : result) {
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch (Exception e) {
