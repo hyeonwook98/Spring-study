c5030a8a7f199e1aa890b3936940a9a981e73ee7
hyeonwook98 <hyeonwook98@naver.com>
2022-07-03 21:15:39 +0900

    chore: 페치 조인(fetch join) 학습

--- jpa-basic/jpql/src/main/java/jpql/JpaMain.java
+++ jpa-basic/jpql/src/main/java/jpql/JpaMain.java
@@ -16,26 +16,49 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
-            member1.setTeam(team);
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select m From Team t join t.members m";
-            Integer result = em.createQuery(query, Integer.class).getSingleResult();
+            String query = "select t From Team t";
+            List<Team> result = em.createQuery(query, Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
+                    .getResultList();
 
-            System.out.println("result = " + result);
+            System.out.println("result = " + result.size());
+
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + "|members=" + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println("-> member = " + member);
+                }
+            }
+
+
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- jpa-basic/jpql/src/main/java/jpql/Team.java
+++ jpa-basic/jpql/src/main/java/jpql/Team.java
@@ -1,5 +1,7 @@
 package jpql;
 
+import org.hibernate.annotations.BatchSize;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
