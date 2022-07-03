368d521892c95f59943f79b7b7998b7680d72c8a
hyeonwook98 <hyeonwook98@naver.com>
2022-07-03 15:43:06 +0900

    chore: JPQL 함수 학습

--- jpa-basic/jpql/src/main/java/jpql/JpaMain.java
+++ jpa-basic/jpql/src/main/java/jpql/JpaMain.java
@@ -15,23 +15,18 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Member member1 = new Member();
+            member1.setUsername("관리자1");
+            em.persist(member1);
 
-            Member member = new Member();
-            member.setUsername(null);
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
-
-            member.setTeam(team);
-
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            String query = "select nullif(m.username, '관리자') as username " + "from Member m ";
+            String query = "select function('group_concat', m.username) From Member m";
             List<String> result = em.createQuery(query, String.class).getResultList();
 
             for (String s : result) {
