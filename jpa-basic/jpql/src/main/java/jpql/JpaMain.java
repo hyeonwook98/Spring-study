70e216fba4242049f233540683ef20dc0f938e88
hyeonwook98 <hyeonwook98@naver.com>
2022-07-03 16:57:29 +0900

    chore: 경로 표현식 학습

--- jpa-basic/jpql/src/main/java/jpql/JpaMain.java
+++ jpa-basic/jpql/src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -15,24 +16,26 @@ public class JpaMain {
 
         try {
 
+            Team team = new Team();
+            em.persist(team);
+
             Member member1 = new Member();
             member1.setUsername("관리자1");
+            member1.setTeam(team);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
+            member2.setTeam(team);
             em.persist(member2);
 
             em.flush();
             em.clear();
 
-            String query = "select function('group_concat', m.username) From Member m";
-            List<String> result = em.createQuery(query, String.class).getResultList();
+            String query = "select m From Team t join t.members m";
+            Integer result = em.createQuery(query, Integer.class).getSingleResult();
 
-            for (String s : result) {
-                System.out.println("s = " + s);
-            }
-
+            System.out.println("result = " + result);
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
