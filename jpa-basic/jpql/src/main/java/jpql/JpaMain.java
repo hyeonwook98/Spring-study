f152dfa6a80c0e23b7bc004e2adbd2c3d3543646
hyeonwook98 <hyeonwook98@naver.com>
2022-07-04 10:27:34 +0900

    chore: 엔티티 직접 사용 학습

--- jpa-basic/jpql/src/main/java/jpql/JpaMain.java
+++ jpa-basic/jpql/src/main/java/jpql/JpaMain.java
@@ -42,22 +42,15 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select t From Team t";
-            List<Team> result = em.createQuery(query, Team.class)
-                    .setFirstResult(0)
-                    .setMaxResults(2)
+            String query = "select m From Member m where m.team = :team";
+            
+            List<Member> members = em.createQuery(query, Member.class)
+                    .setParameter("team",teamA)
                     .getResultList();
 
-            System.out.println("result = " + result.size());
-
-            for (Team team : result) {
-                System.out.println("team = " + team.getName() + "|members=" + team.getMembers().size());
-                for (Member member : team.getMembers()) {
-                    System.out.println("-> member = " + member);
-                }
+            for (Member member : members) {
+                System.out.println("member = " + member);
             }
-
-
 
             tx.commit();
         } catch (Exception e) {
