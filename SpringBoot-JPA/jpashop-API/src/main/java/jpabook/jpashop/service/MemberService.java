5e778bb96dd1f532d9cb65bd023aad83310a64bd
hyeonwook98 <hyeonwook98@naver.com>
2023-01-14 16:38:00 +0900

    feat: 회원 수정 API 재학습

--- SpringBoot-JPA/jpashop-API/src/main/java/jpabook/jpashop/service/MemberService.java
+++ SpringBoot-JPA/jpashop-API/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -38,6 +38,12 @@ public class MemberService {
     public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
     }
+
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
 }
 
 
