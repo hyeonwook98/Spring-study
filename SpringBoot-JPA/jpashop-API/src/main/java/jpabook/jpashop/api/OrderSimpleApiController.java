bd5f0d98758c7ce495068195db88339e532dc273
hyeonwook98 <hyeonwook98@naver.com>
2023-01-14 18:31:34 +0900

    feat: 조회할 때 엔티티를 직접 노출 시 문제점 학습

---
+++ SpringBoot-JPA/jpashop-API/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/*
+ * 컬렉션이 아닌 xToOne 관계에서 성능 최적화에 대해 알아볼 것이다.
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ * */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> orderV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getDelivery().getAddress(); //Lazy 강제 초기화
+        }
+        return all;
+    }
+}
