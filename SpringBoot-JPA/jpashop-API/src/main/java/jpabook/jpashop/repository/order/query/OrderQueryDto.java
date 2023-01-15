9f960a402429ef5c2e1a3d41697d56c089d5873b
hyeonwook98 <hyeonwook98@naver.com>
2023-01-15 23:06:53 +0900

    feat: JPA에서 DTO로 직접 조회,플랫 데이터 최적화 학습

--- SpringBoot-JPA/jpashop-API/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ SpringBoot-JPA/jpashop-API/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -6,6 +6,7 @@ import lombok.EqualsAndHashCode;
 import java.time.LocalDateTime;
 import java.util.List;
 @Data
+@EqualsAndHashCode(of = "orderId")
 public class OrderQueryDto {
     private Long orderId;
     private String name;
@@ -21,5 +22,15 @@ public class OrderQueryDto {
         this.orderDate = orderDate;
         this.orderStatus = orderStatus;
         this.address = address;
+    }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate,
+                         OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
     }
 }
