bb21cd8079723604693d657fdf4b9484b30b18f8
hyeonwook98 <hyeonwook98@naver.com>
2023-01-15 17:44:04 +0900

    feat: 페치 조인 사용하여 컬렉션 조회 학습

--- SpringBoot-JPA/jpashop-API/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ SpringBoot-JPA/jpashop-API/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -98,4 +98,15 @@ public class OrderRepository {
     }
 
 
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                        "select distinct o from Order o" +
+                                " join fetch o.member m" +
+                                " join fetch o.delivery d" +
+                                " join fetch o.orderItems oi" +
+                                " join fetch oi.item i", Order.class)
+                .setFirstResult(1)
+                .setMaxResults(100)
+                .getResultList();
+    }
 }
