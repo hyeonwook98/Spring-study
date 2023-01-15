c9ae7977c75948e138c57d82a0d9b729edc6b89d
hyeonwook98 <hyeonwook98@naver.com>
2023-01-15 15:03:41 +0900

    feat: JPA에서 DTO로 바로 조회하기 학습

--- SpringBoot-JPA/jpashop-API/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ SpringBoot-JPA/jpashop-API/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,9 +5,10 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,7 @@ import java.util.stream.Collectors;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     @GetMapping("/api/v1/simple-orders")
     public List<Order> orderV1() {
@@ -59,6 +61,11 @@ public class OrderSimpleApiController {
                 .collect(Collectors.toList());
 
         return result;
+    }
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> orderV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
     }
 
     @Data
--- SpringBoot-JPA/jpashop-API/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ SpringBoot-JPA/jpashop-API/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -96,4 +96,6 @@ public class OrderRepository {
                         " join fetch o.delivery d", Order.class
         ).getResultList();
     }
+
+
 }
