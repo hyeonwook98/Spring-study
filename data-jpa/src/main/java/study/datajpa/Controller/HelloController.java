af949af012844d3f7cb252709ae025d1cfe3cd18
hyeonwook98 <hyeonwook98@naver.com>
2023-01-16 17:11:43 +0900

    chore: SpringData-JPA 프로젝트 생성

---
+++ data-jpa/src/main/java/study/datajpa/Controller/HelloController.java
@@ -0,0 +1,14 @@
+package study.datajpa.Controller;
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
