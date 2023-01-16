2f0aae4ac60af757f8d286d56768143e0c62cc4a
hyeonwook98 <hyeonwook98@naver.com>
2023-01-16 18:27:44 +0900

    feat: 스프링 데이터 JPA와 DB설정 및 동작확인

--- data-jpa/src/main/java/study/datajpa/controller/HelloController.java
+++ data-jpa/src/main/java/study/datajpa/controller/HelloController.java
@@ -1,6 +1,5 @@
-package study.datajpa.Controller;
+package study.datajpa.controller;
 
-import lombok.Getter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
---
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,30 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성보장
+    }
+}
