78f7af7acd616665b63d9b44c6df0cc465f82db3
hyeonwook98 <hyeonwook98@naver.com>
2023-01-19 17:42:08 +0900

    feat: 사용자 정의 리포지토리 구현

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -268,4 +268,9 @@ public class MemberRepositoryTest {
         //when
         List<Member> result = memberRepository.findLockByUsername("member1");
     }
+
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }
