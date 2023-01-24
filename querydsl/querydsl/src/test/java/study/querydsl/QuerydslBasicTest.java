e42b32206dc9a155fd452a33cf603168d7c233ae
hyeonwook <hyeonwook98@naver.com>
2023-01-24 20:43:55 +0900

    feat: 기본 Q-Type 활용

--- querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -56,11 +56,35 @@ public class QuerydslBasicTest {
     @Test
     public void startQuerydsl() {
         //member1을 찾아라.
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
         QMember m = new QMember("m");
         Member findMember = queryFactory
                 .select(m)
                 .from(m)
                 .where(m.username.eq("member1"))//파라미터 바인딩 처리
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl2() {
+        //member1을 찾아라.
+        QMember m = new QMember("m");
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl3() {
+        //member1을 찾아라.
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetchOne();
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
