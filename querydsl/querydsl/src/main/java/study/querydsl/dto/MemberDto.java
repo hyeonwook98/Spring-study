d95fc0a75c13b674da28b0f46f492733e8abefc9
hyeonwook <hyeonwook98@naver.com>
2023-01-26 18:27:53 +0900

    feat: 프로젝션과 결과 반환 - @QueryProjection

--- querydsl/querydsl/src/main/java/study/querydsl/dto/MemberDto.java
+++ querydsl/querydsl/src/main/java/study/querydsl/dto/MemberDto.java
@@ -1,4 +1,5 @@
 package study.querydsl.dto;
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
 @Data
 public class MemberDto {
@@ -8,6 +9,7 @@ public class MemberDto {
     public MemberDto() {
     }
 
+    @QueryProjection
     public MemberDto(String username, int age) {
         this.username = username;
         this.age = age;
--- querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.QMemberDto;
 import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
@@ -554,7 +555,7 @@ public class QuerydslBasicTest {
             System.out.println("userDto = " + userDto);
         }
     }
-    
+
 
     @Test
     public void findDtoByConstructor() {
@@ -562,6 +563,18 @@ public class QuerydslBasicTest {
                 .select(Projections.constructor(MemberDto.class,
                         member.username,
                         member.age))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+    }
+
+    @Test
+    public void findDtoByQueryProjection() {
+        List<MemberDto> result = queryFactory
+                .select(new QMemberDto(member.username, member.age))
                 .from(member)
                 .fetch();
 
