acbdf23d801cf6be05501543679686874826b988
hyeonwook98 <hyeonwook98@naver.com>
2023-01-28 23:39:50 +0900

    feat: 사용자 정의 리포지토리 학습

--- querydsl/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ querydsl/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -28,8 +28,33 @@ class MemberRepositoryTest {
 
         List<Member> result1 = memberRepository.findAll();
         assertThat(result1).containsExactly(member);
-        
+
         List<Member> result2 = memberRepository.findByUsername("member1");
         assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    public void searchTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberRepository.search(condition);
+        assertThat(result).extracting("username").containsExactly("member4");
     }
 }
