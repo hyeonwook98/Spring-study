7b40130a860fba855234c48480be3ddfb9f068b2
hyeonwook98 <hyeonwook98@naver.com>
2023-01-29 22:06:03 +0900

    feat: Query By Example 학습

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -4,9 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -292,5 +290,33 @@ public class MemberRepositoryTest {
 
         //then
         Assertions.assertThat(result.size()).isEqualTo(1);
+    }
+
+    @SpringBootTest
+    @Transactional
+    public class QueryByExampleTest {
+        @Test
+        public void basic() throws Exception {
+            //given
+            Team teamA = new Team("teamA");
+            em.persist(teamA);
+            em.persist(new Member("m1", 0, teamA));
+            em.persist(new Member("m2", 0, teamA));
+            em.flush();
+
+            //when
+            //Probe 생성
+            Member member = new Member("m1");
+            Team team = new Team("teamA"); //내부조인으로 teamA 가능
+            member.setTeam(team);
+
+            //ExampleMatcher 생성, age 프로퍼티는 무시
+            ExampleMatcher matcher = ExampleMatcher.matching().withIgnorePaths("age");
+            Example<Member> example = Example.of(member, matcher);
+            List<Member> result = memberRepository.findAll(example);
+
+            //then
+            assertThat(result.size()).isEqualTo(1);
+        }
     }
 }
