47760511e3411eaf0a7dae7b700d8c8b2603b35e
hyeonwook98 <hyeonwook98@naver.com>
2023-01-31 17:13:44 +0900

    feat: jwt Bearer 인증 방식을 위한 설정

--- jwt-server/src/main/java/pratice/jwtserver/config/SecurityConfig.java
+++ jwt-server/src/main/java/pratice/jwtserver/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                 .and()
                 .addFilter(corsFilter) //CorsConfig에서 만든 필터정보를 넣어준다. 이로써 모든 요청이 이 필터를 거친다.
                 .formLogin().disable() //jwt토큰을 사용한 서버니까 폼로그인 형식으로 id,pw을 받지않는다.
-                .httpBasic().disable()
+                .httpBasic().disable() //매 요청시마다 id,password 를 헤더에 담고 가는 방식인 httpBasic을 사용안하겠다. -> 이 방법은 id,pw 암호화가 안되서 노출이 될수있음. 그래서 https를 사용
                 .authorizeRequests()
                 .antMatchers("/api/v1/user/**")
                 .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
