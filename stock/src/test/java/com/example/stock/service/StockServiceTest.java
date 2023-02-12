0aaa63ae3ebbcd56f10420813fe11b4920a5f752
hyeonwook98 <hyeonwook98@naver.com>
2023-02-12 22:11:44 +0900

    bug: 동시성 이슈 문제 확인

--- stock/src/test/java/com/example/stock/service/StockServiceTest.java
+++ stock/src/test/java/com/example/stock/service/StockServiceTest.java
@@ -5,9 +5,14 @@ import com.example.stock.repository.StockRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class StockServiceTest {
@@ -39,5 +44,32 @@ class StockServiceTest {
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         Assertions.assertEquals(99,stock.getQuantity());
+    }
+
+    @Test
+    @DisplayName("동시에_100개의_요청")
+    public void sameTime_100_Request() throws InterruptedException {
+        int threadCount = 100;
+        //멀티스레드 이용 ExecutorService : 비동기를 단순하게 처리할 수 있도록 해주는 java api
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        //다른 스레드에서 수행이 완료될 때 까지 대기할 수 있도록 도와주는 API - 요청이 끝날때 까지 기다림
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (1 * 100) = 0
+        Assertions.assertEquals(0L, stock.getQuantity());
     }
 }
