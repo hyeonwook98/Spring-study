e98c1acaec27ad1ef432030ca5518d78b0decba0
hyeonwook98 <hyeonwook98@naver.com>
2023-02-12 22:17:01 +0900

    feat: Synchronized 사용해서 동시성 이슈 문제 해결

--- stock/src/main/java/com/example/stock/service/StockService.java
+++ stock/src/main/java/com/example/stock/service/StockService.java
@@ -3,6 +3,7 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StockService {
@@ -13,7 +14,8 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    public void decrease(Long id, Long quantity) {
+//    @Transactional
+    public synchronized void decrease(Long id, Long quantity) {
         // get stock
         Stock stock = stockRepository.findById(id).orElseThrow();
 
