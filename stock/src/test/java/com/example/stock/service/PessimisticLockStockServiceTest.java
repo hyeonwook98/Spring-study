2e344c91fb6bee0d501cdea359190f62b193a830
hyeonwook98 <hyeonwook98@naver.com>
2023-02-13 15:29:21 +0900

    feat: Pessimistic Lock 학습

---
+++ stock/src/test/java/com/example/stock/service/PessimisticLockStockServiceTest.java
@@ -0,0 +1,75 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SpringBootTest
+class PessimisticLockStockServiceTest {
+
+    @Autowired
+    private PessimisticLockStockService pessimisticLockStockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void before() {
+        Stock stock = new Stock(1L, 100L);
+
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void stock_decrease() {
+        pessimisticLockStockService.decrease(1L,1L);
+
+        // 100 - 1 = 99 여야 한다.
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        Assertions.assertEquals(99,stock.getQuantity());
+    }
+
+    @Test
+    @DisplayName("동시에_100개의_요청")
+    public void sameTime_100_Request() throws InterruptedException {
+        int threadCount = 100;
+        //멀티스레드 이용 ExecutorService : 비동기를 단순하게 처리할 수 있도록 해주는 java api
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        //다른 스레드에서 수행이 완료될 때 까지 대기할 수 있도록 도와주는 API - 요청이 끝날때 까지 기다림
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    pessimisticLockStockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (1 * 100) = 0
+        Assertions.assertEquals(0L, stock.getQuantity());
+    }
+}
